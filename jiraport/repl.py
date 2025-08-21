44e74036ee970d21af3903855b3f56f038cb4fba
Richard Howard <richard.howard@wonderschool.com>
2025-08-21 17:05:47 -0400

    Add repl

---
+++ jiraport/repl.py
@@ -0,0 +1,16 @@
+# ruff: noqa
+
+import logging
+import os
+
+import IPython
+from click import Context
+
+from jiraport.main import _jira_connect
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == "__main__":
+    jira = _jira_connect(email=os.environ["JIRA_EMAIL"], server=os.environ["JIRA_SERVER"], token=os.environ["JIRA_TOKEN"])
+    IPython.embed()
