37f8a38a048bdb6784a50bdc8cff6231d65dc0c1
Richard Howard <richard.howard@wonderschool.com>
2025-08-21 08:50:23 -0400

    Add more fields

--- main.py
+++ main.py
@@ -36,50 +36,63 @@ class IssueSummary:
     id: str
     status: str
     story_points: Optional[str]
+
     time_blocked: pendulum.Duration
+    time_dev: pendulum.Duration
+
     date_created: Optional[pendulum.DateTime]
-    date_in_progress: Optional[pendulum.DateTime]
+    date_in_dev: Optional[pendulum.DateTime]
     date_code_review: Optional[pendulum.DateTime]
     date_done: Optional[pendulum.DateTime]
 
 
 def summarize(issue: Issue) -> IssueSummary:
-    date_created = cast(pendulum.DateTime, pendulum.parse(issue.fields.created))
     story_points = "todo"
     time_blocked = pendulum.Duration()
-    blocked_start = None
-    date_in_progress = None
+    time_dev = pendulum.Duration()
+
+    date_created = cast(pendulum.DateTime, pendulum.parse(issue.fields.created))
+    date_previous = date_created
+    date_blocked_start = None
+    date_in_dev = None
     date_code_review = None
     date_done = None
 
     sort_key = lambda h: pendulum.parse(h.created)
 
     for history in sorted(issue.changelog.histories, key=sort_key):
-        dt = cast(pendulum.DateTime, pendulum.parse(history.created))
-        dt = TZ.convert(dt)
+        date_current = cast(pendulum.DateTime, pendulum.parse(history.created))
+        date_current = TZ.convert(date_current)
 
         for item in history.items:
             if item.field == "status":
-                if blocked_start and item.toString != "Blocked":
-                    time_blocked += (dt - blocked_start)
-                    blocked_start = None
+                if date_blocked_start and item.toString != "Blocked":
+                    time_blocked += (date_current - date_blocked_start)
+                    date_blocked_start = None
                 elif item.toString == "Blocked":
-                    blocked_start = dt
+                    date_blocked_start = date_current
+
+                if item.fromString in IN_PROGRESS_STATUSES:
+                    time_dev += (date_current - date_previous)
 
                 if item.toString in IN_PROGRESS_STATUSES:
-                    date_in_progress = dt
+                    date_in_dev = date_current
                 elif item.toString == "Code Review":
-                    date_code_review = dt
+                    date_code_review = date_current
                 elif item.toString == "Done":
-                    date_done = dt
+                    date_done = date_current
+
+        date_previous = date_current
+
 
     return IssueSummary(
         id=issue.key,
         status=issue.fields.status.name,
         story_points=story_points,
         time_blocked=time_blocked,
+        time_dev=time_dev,
         date_created=date_created,
-        date_in_progress=date_in_progress,
+        date_in_dev=date_in_dev,
         date_code_review=date_code_review,
         date_done=date_done,
     )
@@ -89,7 +102,7 @@ def to_csv_row(summary: IssueSummary):
     fields = [
         summary.id,
         summary.story_points,
-        hr_date(summary.date_in_progress),
+        hr_date(summary.date_in_dev),
         hr_date(summary.date_code_review),
         hr_date(summary.date_done)
     ]
@@ -104,27 +117,29 @@ def hr_date(dt: Optional[pendulum.DateTime]):
 def print_table(summaries: list[IssueSummary]):
     table = Table(title="Issue Summaries", expand=True)
     table.add_column("ID", no_wrap=True)
-    table.add_column("Status", no_wrap=True)
     table.add_column("Story Points", no_wrap=True)
-    table.add_column("Time Blocked", no_wrap=True)
-    table.add_column("Created Date", no_wrap=True)
-    table.add_column("In Progress Date", no_wrap=True)
+    table.add_column("In Dev Date", no_wrap=True)
     table.add_column("Code Review Date", no_wrap=True)
     table.add_column("Done Date", no_wrap=True)
+    table.add_column("Blocked?", no_wrap=True)
+    table.add_column("Time Blocked", no_wrap=True)
+    table.add_column("Time In Dev", no_wrap=True)
+    table.add_column("Time In Dev + Blocked", no_wrap=True)
 
     for summary in summaries:
         table.add_row(
             summary.id,
-            summary.status,
-            str(summary.story_points) if summary.story_points else "",
-            str(summary.time_blocked) if summary.time_blocked else "",
-            hr_date(summary.date_created),
-            hr_date(summary.date_in_progress),
+            summary.story_points,
+            hr_date(summary.date_in_dev),
             hr_date(summary.date_code_review),
-            hr_date(summary.date_done)
+            hr_date(summary.date_done),
+            "Yes" if summary.time_blocked.total_seconds() > 0 else "No",
+            str(summary.time_blocked) if summary.time_blocked.total_seconds() > 0 else "",
+            str(summary.time_dev),
+            str(summary.time_dev + summary.time_blocked),
         )
 
-    console = Console(width=120)
+    console = Console(width=200)
     console.print(table)
 
 
