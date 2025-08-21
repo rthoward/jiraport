0c117b1f3878da7f73543ec298f4a8eabade7f77
Richard Howard <richard.howard@wonderschool.com>
2025-08-20 21:03:03 -0400

    WIP

--- main.py
+++ main.py
@@ -12,60 +12,88 @@ EMAIL = os.environ["JIRA_EMAIL"]
 API_TOKEN = os.environ["JIRA_TOKEN"]
 SERVER = os.environ["JIRA_SERVER"]
 
-TIMEZONE = pendulum.timezone("America/New_York")
-ISSUE_FIELDS = "status,changelog"
+TZ = pendulum.timezone("America/New_York")
+ISSUE_FIELDS = "status,changelog,created"
+IN_PROGRESS_STATUSES = {"Development"}
 
 JQL = """
 type = Story AND labels IN (G-DSP, G-SSP, G-Platform, G-Data) AND labels NOT IN (Cadent) AND status = Done AND project = GCM
 """
 
-def transition_time(issue, from_status, to_status):
-    ...
-
-
 def main():
     j = JIRA(server=SERVER, basic_auth=(EMAIL, API_TOKEN))
 
-    # issues = j.search_issues(JQL, maxResults=1, expand="changelog")
-    # issue = issues[0]
-    issue = j.issue("GCM-2082", expand="changelog", fields=ISSUE_FIELDS)
-    summarize(issue)
+    issues = j.search_issues(JQL, maxResults=1, expand="changelog")
 
-
-
-# class Status(Enum):
-#     DEVELOPMENT = "Development"
-#     PRODUCT_ACCEPTANCE = "Product Acceptance"
-#     DONE = "Done"
+    for issue in issues:
+        summary = summarize(issue)
+        print(summary)
+        print(to_csv_row(summary))
 
 
 @dataclass
 class IssueSummary:
-    current_status: Status
-    story_points: int
+    id: str
+    status: str
+    story_points: Optional[str]
     time_blocked: Optional[pendulum.Duration]
-    date_in_progress: Optional[pendulum.Date]
-    date_code_review: Optional[pendulum.Date]
-    date_done: Optional[pendulum.Date]
+    date_created: Optional[pendulum.DateTime]
+    date_in_progress: Optional[pendulum.DateTime]
+    date_code_review: Optional[pendulum.DateTime]
+    date_done: Optional[pendulum.DateTime]
 
 
-def summarize(issue: Issue) -> Optional[IssueSummary]:
-    current_status: Status = issue.fields.status
-    story_points: int
-    time_blocked: Optional[pendulum.Duration]
-    date_in_progress: Optional[pendulum.Date]
-    date_code_review: Optional[pendulum.Date]
-    date_done: Optional[pendulum.Date]
+def summarize(issue: Issue) -> IssueSummary:
+    date_created = cast(pendulum.DateTime, pendulum.parse(issue.fields.created))
+    story_points = "todo"
+    time_blocked: Optional[pendulum.Duration] = None
+    date_in_progress = None
+    date_code_review = None
+    date_done = None
 
     sort_key = lambda h: pendulum.parse(h.created)
 
     for history in sorted(issue.changelog.histories, key=sort_key):
         dt = cast(pendulum.DateTime, pendulum.parse(history.created))
+        dt = TZ.convert(dt)
 
         for item in history.items:
             if item.field == "status":
-                print(f"[{TIMEZONE.convert(dt)}] {item.fromString} -> {item.toString}")
+                print(f"[{dt}] {item.fromString} -> {item.toString}")
 
+                if item.toString in IN_PROGRESS_STATUSES:
+                    date_in_progress = dt
+                elif item.toString == "Code Review":
+                    date_code_review = dt
+                elif item.toString == "Done":
+                    date_done = dt
+
+    return IssueSummary(
+        id=issue.id,
+        status=issue.fields.status.name,
+        story_points=story_points,
+        time_blocked=time_blocked,
+        date_created=date_created,
+        date_in_progress=date_in_progress,
+        date_code_review=date_code_review,
+        date_done=date_done,
+    )
+
+
+def to_csv_row(summary: IssueSummary):
+    fields = [
+        summary.id,
+        summary.story_points,
+        hr_date(summary.date_in_progress),
+        hr_date(summary.date_code_review),
+        hr_date(summary.date_done)
+    ]
+
+    return ",".join(fields)
+
+
+def hr_date(dt: Optional[pendulum.DateTime]):
+    return dt.format("MM/DD/YYYY") if dt else ""
 
 if __name__ == "__main__":
     main()
