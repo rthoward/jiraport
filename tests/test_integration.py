2e610801d1c0f6cb3fd7bc2229eac86220695962
Richard Howard <richard.howard@wonderschool.com>
2025-08-23 09:02:28 -0400

    Use pytest-vcr

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -9,75 +9,74 @@ from jiraport.utils import TZ
 
 @pytest.mark.integration
 @pytest.mark.vcr
-def test_jira_integration_with_vcr(jira_config, vcr_config):
-    with vcr.VCR(**vcr_config).use_cassette("test_jira_integration.yaml"):
-        jira_client = _jira_connect(**jira_config)
+def test_jira_integration(jira_config):
+    jira_client = _jira_connect(**jira_config)
 
-        jira_issues = jira_client.search_issues(
-            DEFAULT_JQL,
-            maxResults=5,  # Limit results for testing
-            expand="changelog",
-            fields="id,created,status",
-        )
+    jira_issues = jira_client.search_issues(
+        DEFAULT_JQL,
+        maxResults=5,
+        expand="changelog",
+        fields="id,created,status",
+    )
 
-        summaries = [issues.summarize(issue) for issue in jira_issues]
+    summaries = [issues.summarize(issue) for issue in jira_issues]
 
-        assert summaries == [
-            issues.IssueSummary(
-                id="GCM-2237",
-                status="Done",
-                story_points="",
-                time_blocked=Duration(),
-                time_dev=Duration(days=1, minutes=44, seconds=49, microseconds=838000),
-                date_created=DateTime(2025, 8, 19, 6, 56, 15, 157000, tzinfo=TZ),
-                date_in_dev=DateTime(2025, 8, 19, 7, 0, 19, 550000, tzinfo=TZ),
-                date_code_review=DateTime(2025, 8, 20, 7, 44, 26, 148000, tzinfo=TZ),
-                date_done=DateTime(2025, 8, 20, 15, 44, 49, 52000, tzinfo=TZ),
+    assert summaries == [
+        issues.IssueSummary(
+            id="GCM-2237",
+            status="Done",
+            story_points="",
+            time_blocked=Duration(),
+            time_dev=Duration(days=1, minutes=44, seconds=49, microseconds=838000),
+            date_created=DateTime(2025, 8, 19, 6, 56, 15, 157000, tzinfo=TZ),
+            date_in_dev=DateTime(2025, 8, 19, 7, 0, 19, 550000, tzinfo=TZ),
+            date_code_review=DateTime(2025, 8, 20, 7, 44, 26, 148000, tzinfo=TZ),
+            date_done=DateTime(2025, 8, 20, 15, 44, 49, 52000, tzinfo=TZ),
+        ),
+        issues.IssueSummary(
+            id="GCM-2082",
+            status="Done",
+            story_points="",
+            time_blocked=Duration(),
+            time_dev=Duration(
+                days=3, hours=21, minutes=47, seconds=55, microseconds=262000
             ),
-            issues.IssueSummary(
-                id="GCM-2082",
-                status="Done",
-                story_points="",
-                time_blocked=Duration(),
-                time_dev=Duration(
-                    days=3, hours=21, minutes=47, seconds=55, microseconds=262000
-                ),
-                date_created=DateTime(2025, 8, 13, 9, 3, 59, 971000, tzinfo=TZ),
-                date_in_dev=DateTime(2025, 8, 14, 9, 0, 25, 497000, tzinfo=TZ),
-                date_code_review=None,
-                date_done=DateTime(2025, 8, 18, 6, 48, 20, 759000, tzinfo=TZ),
-            ),
-            issues.IssueSummary(
-                id="GCM-2054",
-                status="Done",
-                story_points="",
-                time_blocked=Duration(),
-                time_dev=Duration(),
-                date_created=DateTime(2025, 8, 12, 11, 44, 54, 246000, tzinfo=TZ),
-                date_in_dev=None,
-                date_code_review=None,
-                date_done=DateTime(2025, 8, 18, 8, 47, 44, 51000, tzinfo=TZ),
-            ),
-            issues.IssueSummary(
-                id="GCM-2053",
-                status="Done",
-                story_points="",
-                time_blocked=Duration(),
-                time_dev=Duration(hours=1, minutes=22, seconds=4, microseconds=54000),
-                date_created=DateTime(2025, 8, 12, 11, 26, 34, 992000, tzinfo=TZ),
-                date_in_dev=DateTime(2025, 8, 18, 7, 25, 36, 12000, tzinfo=TZ),
-                date_code_review=DateTime(2025, 8, 18, 7, 25, 36, 12000, tzinfo=TZ),
-                date_done=DateTime(2025, 8, 18, 8, 47, 40, 66000, tzinfo=TZ),
-            ),
-            issues.IssueSummary(
-                id="GCM-2048",
-                status="Done",
-                story_points="",
-                time_blocked=Duration(),
-                time_dev=Duration(),
-                date_created=DateTime(2025, 8, 12, 7, 24, 3, 268000, tzinfo=TZ),
-                date_in_dev=None,
-                date_code_review=None,
-                date_done=DateTime(2025, 8, 13, 9, 0, 59, 137000, tzinfo=TZ),
-            ),
-        ]
+            date_created=DateTime(2025, 8, 13, 9, 3, 59, 971000, tzinfo=TZ),
+            date_in_dev=DateTime(2025, 8, 14, 9, 0, 25, 497000, tzinfo=TZ),
+            date_code_review=None,
+            date_done=DateTime(2025, 8, 18, 6, 48, 20, 759000, tzinfo=TZ),
+        ),
+        issues.IssueSummary(
+            id="GCM-2054",
+            status="Done",
+            story_points="",
+            time_blocked=Duration(),
+            time_dev=Duration(),
+            date_created=DateTime(2025, 8, 12, 11, 44, 54, 246000, tzinfo=TZ),
+            date_in_dev=None,
+            date_code_review=None,
+            date_done=DateTime(2025, 8, 18, 8, 47, 44, 51000, tzinfo=TZ),
+        ),
+        issues.IssueSummary(
+            id="GCM-2053",
+            status="Done",
+            story_points="",
+            time_blocked=Duration(),
+            time_dev=Duration(hours=1, minutes=22, seconds=4, microseconds=54000),
+            date_created=DateTime(2025, 8, 12, 11, 26, 34, 992000, tzinfo=TZ),
+            date_in_dev=DateTime(2025, 8, 18, 7, 25, 36, 12000, tzinfo=TZ),
+            date_code_review=DateTime(2025, 8, 18, 7, 25, 36, 12000, tzinfo=TZ),
+            date_done=DateTime(2025, 8, 18, 8, 47, 40, 66000, tzinfo=TZ),
+        ),
+        issues.IssueSummary(
+            id="GCM-2048",
+            status="Done",
+            story_points="",
+            time_blocked=Duration(),
+            time_dev=Duration(),
+            date_created=DateTime(2025, 8, 12, 7, 24, 3, 268000, tzinfo=TZ),
+            date_in_dev=None,
+            date_code_review=None,
+            date_done=DateTime(2025, 8, 13, 9, 0, 59, 137000, tzinfo=TZ),
+        ),
+    ]
